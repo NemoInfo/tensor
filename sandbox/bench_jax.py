5b962bfad186e3d2f828dc20e785012b32ccf8ef
Aaron <aaronpanaitescu@gmail.com>
2026-05-11 19:23:57 +0100

    Add jax benchmark
    Signed-off-by: Aaron <aaronpanaitescu@gmail.com>

---
+++ sandbox/bench_jax.py
@@ -0,0 +1,25 @@
+import jax
+import jax.numpy as jnp
+from time import time
+
+N = 1024
+I = 1000
+
+a = jnp.arange(N * N, dtype=jnp.float32).reshape((N, N))
+b = jnp.ones((N, N), dtype=jnp.float32)
+c = jnp.ones((N, N), dtype=jnp.float32)
+
+@jax.jit
+def compute_r(a, b, c):
+    return (a + (c + b) + (a + b)) + ((a + b) + (b + c))
+
+if __name__ == "__main__":
+    _ = compute_r(a, b, c).block_until_ready()
+
+    start = time()
+    for _ in range(I):
+        r = compute_r(a, b, c)
+        r.block_until_ready()          # ensure the computation is finished
+    dt = time() - start
+
+    print(f"jax: {I / dt:.6f} it/s ({int(dt * 1000):,} ms elapsed)")
